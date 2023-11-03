d5d2ed096386aaa011b4d6924019664b5cf93741
shivamduhan <shivamduhanx@gmail.com>
2023-11-03 17:07:20 -0400

    added loop fusion

--- perf.c
+++ perf.c
@@ -1,6 +1,16 @@
+#include <math.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
 int  main(){
 int **matrix = (int **)malloc(50000 * sizeof(int*));
 for(int i = 0; i < 50000; i++) {
@@ -41,6 +51,21 @@ int good_e = clock();
 for(int i = 0; i < 50000; i++) {
     free(matrix[i]);
 }
+//loop 
+int fission_s = clock();
+for (int i = 0; i < 10000; i++) {
+arr3[i] = min(arr1[i],arr2[i]);}
+
+for(int i = 0; i < 10000; i++) {
+arr4[i] = max(arr1[i], arr2[i]);}
+int fission_e = clock();
+
+int fusion_s = clock();
+for(int i = 0; i < 10000; i++) {
+arr3[i] = min(arr1[i], arr2[i]);
+arr4[i] = max(arr1[i], arr2[i]);}
+int fusion_e = clock();
+
 free(matrix);
 free(arr1);
 free(arr2);
@@ -50,5 +75,7 @@ free(arr4);
 double speedup = 1/(((double)(good_e - good_s)) / ((double)(poor_e - poor_s)));
 printf("Loop interchange achieved a speedup of %f\n", speedup);
 
-return 0;
-}
+double fusion = 1/(((double)(fusion_e - fusion_s)) / ((double)(fission_e - fission_s)));
+printf("Loop fusion achieved a speedup of %f\n", fusion);
+
+return 0;}
